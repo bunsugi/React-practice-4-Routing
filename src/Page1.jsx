f4090e6b398e36460e28d17c79a360a0ea51fdfa
ay-sugiura <65064434+ay-sugiura@users.noreply.github.com>
2021-08-14 17:36:37 +0900

    ネストされたルーティング

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1ページです</h1>
+      <Link to="/page1/detailA">詳細A</Link>
+      <br />
+      <Link to="/page1/detailB">詳細B</Link>
     </div>
   );
 };
